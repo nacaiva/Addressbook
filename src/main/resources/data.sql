5fa14219e0bb45acf56eadff6192ff1aeeabc22c
Natalija <natalija.ivanovic@qacube.com>
2016-12-07 15:21:28 +0100

    Few more phone numbers

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,12 @@
-insert into PERSON (ID, FIRSTNAME, LASTNAME) values (1,'Petar', 'Petrovic');
-insert into PERSON (FIRSTNAME, LASTNAME) values ('Jovan', 'Jovic');
-insert into PERSON (FIRSTNAME, LASTNAME) values ('Zika', 'Zivkovic');
-insert into PERSON (FIRSTNAME, LASTNAME) values ('Ana', 'Bozic');
-insert into PERSON (FIRSTNAME, LASTNAME) values ('Milica', 'Ilic');
+insert into PERSON (ID, FIRSTNAME, LASTNAME) values (1, 'Petar', 'Petrovic');
+insert into PERSON (ID, FIRSTNAME, LASTNAME) values (2, 'Jovan', 'Jovic');
+insert into PERSON (ID, FIRSTNAME, LASTNAME) values (3, 'Zika', 'Zivkovic');
+insert into PERSON (ID, FIRSTNAME, LASTNAME) values (4, 'Ana', 'Bozic');
+insert into PERSON (ID, FIRSTNAME, LASTNAME) values (5, 'Milica', 'Ilic');
+insert into PHONE (PERSONID, NUMBER) values (1,'065/2819417');
+insert into PHONE (PERSONID, NUMBER) values (1,'069/8294145');
+insert into PHONE (PERSONID, NUMBER) values (2,'061/2819417');
+insert into PHONE (PERSONID, NUMBER) values (3,'065/2819417');
 insert into PHONE (PERSONID, NUMBER) values (1,'065/2819417');
 
 
