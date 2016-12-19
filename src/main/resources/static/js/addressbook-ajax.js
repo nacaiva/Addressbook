01e0039e754f122c62d6d0b9801e0b90fa388e78
Natalija <natalija.ivanovic@qacube.com>
2016-12-19 11:35:58 +0100

    css style in ajax page

--- src/main/resources/static/js/addressbook-ajax.js
+++ src/main/resources/static/js/addressbook-ajax.js
@@ -4,19 +4,37 @@ $(function() {
   }).done(function(entries) {
     var $list = $('#addressbook');
     var personTemp = $list.find('#personTemp').html();
-    var detailsTemp = $list.find('#detailsTemp').html();
+    var numberTemp = $list.find('#numberTemp').html();
+    var addressTemp = $list.find('#addressTemp').html();
+    var emailTemp = $list.find('#emailTemp').html();
+    var noteTemp = $list.find('#noteTemp').html();
 
     $.each(entries, function(index) {
       var entry = entries[index];
 
       var detailsValue='';
+      var numbers= '';
+      var addresses= '';
+      var emails= '';
+      var notes= '';
       $.each(entry.details, function(index) {
         var detail = entry.details[index];
-        detailsValue+=detailsTemp.replace('{{detailValue}}',detail.type + ': ' +detail.value);
+        
+        if (detail.type.match('NUMBER')){
+          numbers+=numberTemp.replace('{{number}}', detail.value);
+        }else if (detail.type.match('ADDRESS')){
+          addresses+=addressTemp.replace('{{address}}', detail.value);
+        }else if (detail.type.match('EMAIL')){
+          emails+=emailTemp.replace('{{email}}', detail.value);
+        }else if (detail.type.match('NOTE')){
+          notes+=noteTemp.replace('{{note}}', detail.value);
+        }
+        
       });
       
       $list.append(
-          personTemp.replace('{{name}}',entry.name).replace('{{details}}',detailsValue)
+          personTemp.replace('{{name}}',entry.name).replace('{{numbers}}',numbers).replace('{{addresses}}',addresses)
+          .replace('{{emails}}',emails).replace('{{notes}}',notes)
       );
     });
   });
